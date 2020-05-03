6bc1f1fd62ba3f8f8bd732c3304a2315c6b1d9bb
Nicholas Burrell <nicholas.burrell@protonmail.com>
2020-05-03 03:03:25 -0700

    Add patch for Unicorn CMake build for MinGW

--- cmake/modules/FindUnicorn.cmake
+++ cmake/modules/FindUnicorn.cmake
@@ -21,6 +21,36 @@ if(UNICORN_FOUND)
 		INTERFACE_INCLUDE_DIRECTORIES ${LIBUNICORN_INCLUDE_DIR}
 		INTERFACE_LINK_LIBRARIES Threads::Threads
 		)
+elseif(FETCH_MISSING)
+	find_package(Patch)
+	if(Patch_FOUND)
+		message(STATUS "Patch found" )
+	endif()
+
+	set(UNICORN_VERSON "1.0.2-rc3")
+	set(UNICORN_CONFIGURE_COMMAND "")
+	set(UNICORN_BUILD_COMMAND "")
+	if(WIN32)
+		if(MSVC)
+			set(UNICORN_CONFIGURE_COMMAND )
+		else()
+
+		endif()
+	else()
+
+	endif()
+	FetchContent_Declare(
+			unicorn
+			GIT_REPOSITORY 		https://github.com/unicorn-engine/unicorn.git
+			GIT_TAG 			${UNICORN_VERSION}
+			PATCH_COMMAND       ${Patch_EXECUTABLE} ${FETCHCONTENT_BASE_DIR}/unicorn-src/CMakeLists.txt < ${PROJECT_SOURCE_DIR}/patches/unicorn-git.patch
+			CONFIGURE_COMMAND 	${UNICORN_CONFIGURE_COMMAND}
+			BUILD_COMMAND 		${UNICORN_BUILD_COMMAND}
+	)
+	FetchContent_MakeAvailable(unicorn)
+	add_library(Unicorn::Unicorn INTERFACE)
+	set_target_properties(Unicorn::Unicorn PROPERTIES
+			INTERFACE_LINK_LIBRARIES unicorn Threads::Threads)
 endif()
 
 mark_as_advanced(LIBUNICORN_INCLUDE_DIR LIBUNICORN_LIBRARY)
